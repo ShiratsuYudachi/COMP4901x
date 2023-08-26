dc0208d219a13bbea6ec6645ba8072411b0ecbc8
Tsuyue <53194250+ShiratsuYudachi@users.noreply.github.com>
2023-08-27 03:53:52 +0800

    remake COMP2012H

--- COMP2012H/PA11/compile.bat
+++
@@ -1,6 +0,0 @@
-g++ -c dictionary.cpp
-g++ -c main.cpp
-g++ -c node.cpp
-g++ -c tasks.cpp
-
-g++ dictionary.o main.o node.o tasks.o -o dictionary
--- COMP2012H/PA8/Terrains/EmptyLand.cpp
+++
@@ -1,7 +0,0 @@
-#include "EmptyLand.h"
-
-//write your codes here
-EmptyLand::EmptyLand(int x, int y) : Terrain(x, y) {}
-EmptyLand::~EmptyLand() {}
-bool EmptyLand::is_blocked() const { return false; }
-string EmptyLand::get_display_str() const { return SYM_EMPTY; }
